8ec31e19a21e57fe9d8f2187b35f9d65bd29feab
jhi2 <johndrostan@outlook.com>
2023-08-10 15:33:15 -0700

    Add files via upload

---
+++ calcgame1.0.py
@@ -0,0 +1,22 @@
+print('time to add!!!!!!!')
+question1 = int(input ('what is 1 + 1\n'))
+
+if question1 == 2:
+  print('correct')
+else:
+  print('bad')
+question2 = int(input ('what is 1+2\n'))
+
+if question2 == 3:
+  print('correct')
+  print('and so on')
+else:
+  print('bad')
+input('press enter to exit\n')
+
+exit()
+
+
+
+
+
